9a98968b90888801ab99187b7ba0e4ba40442688
broggi_t <broggi_t@epitech.eu>
2015-02-25 01:44:53 +0100

    improve display and fix eating
    	philosophers can't eat more rations than existing
    	adding macros for compile-time configuration
    	adding colors to make display more readable
    	1 color = 1 philosopher = 1 thread
    	making column-display (one column per action)

--- philosophe.h
+++ philosophe.h
@@ -13,23 +13,45 @@
 
 # include <pthread.h>
 
-# define N_PHI 7
+# ifndef NB_PHILO
+#  define NB_PHILO	7
+# endif
+
+# ifndef NB_FOOD
+#  define NB_FOOD	1000
+# endif
+
+# define MIN_TIME	1000
+
+# ifndef TIME_ACTION
+#  define TIME_ACTION	20
+# endif
+
+typedef struct		s_stat
+{
+  pthread_mutex_t	food_lock;
+  int			food;
+  int			total_eaten;
+}			t_stat;
 
 typedef struct		s_data
 {
   pthread_t		thread;
   pthread_mutex_t	chopstick;
   int			id;
+  int			eaten_plates;
   struct s_data		*phi_st;
-  int			food;
+  struct s_stat		*stat;
 }			t_data;
 
 # ifndef EXIT_SUCCESS
-#  define EXIT_SUCCESS 0
+#  define EXIT_SUCCESS	0
 # endif
 
 # ifndef EXIT_FAILURE
-#  define EXIT_FAILURE 1
+#  define EXIT_FAILURE	1
 # endif
+
+# define MAX(x, y)	(x < y ? y : x)
 
 #endif /* !PHILOSOPHE_H_ */
