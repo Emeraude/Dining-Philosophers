06256c385eb1bb7412cca19e1ac5a02059a98283
broggi_t <broggi_t@epitech.eu>
2015-02-25 01:44:53 +0100

    improve display and fix eating
    	philosophers can't eat more rations than existing
    	adding macros for compile-time configuration
    	adding colors to make display more readable
    	1 color = 1 philosopher = 1 thread
    	making column-display (one column per action)

--- philosophe.c
+++ philosophe.c
@@ -12,81 +12,103 @@
 #include <stdio.h>
 #include "philosophe.h"
 
-void		rest(t_data *data)
+static void	rest(t_data *data)
 {
-  usleep(10000);
-  printf("[%d] rests\n", data->id);
+  printf("\033[0;3%dm[%d] rests\033[0m\n", data->id % 8 + 1, data->id);
+  usleep(MAX(TIME_ACTION, 1) * MIN_TIME);
 }
 
-void		eat(t_data *data, int *count)
+static void	eat(t_data *data)
 {
-  *count = *count + 1;
-  data->phi_st->food--;
-  printf("[%d] eats\n", data->id);
-  usleep(10000);
-  pthread_mutex_unlock(&data->phi_st[data->id % N_PHI].chopstick);
-  pthread_mutex_unlock(&data->phi_st[(data->id + 1) % N_PHI].chopstick);
+  if (!pthread_mutex_lock(&data->stat->food_lock))
+    {
+      if (!data->stat->food)
+	{
+	  pthread_mutex_unlock(&data->stat->food_lock);
+	  pthread_mutex_unlock(&data->phi_st[data->id % NB_PHILO].chopstick);
+	  pthread_mutex_unlock(&data->phi_st[(data->id + 1) % NB_PHILO].chopstick);
+	  pthread_exit(NULL);
+	}
+      --data->stat->food;
+      pthread_mutex_unlock(&data->stat->food_lock);
+    }
+  ++data->eaten_plates;
+  ++data->stat->total_eaten;
+  printf("\033[0;3%dm\t\t[%d] eats\033[0m\n", data->id % 8 + 1, data->id);
+  usleep(MAX(TIME_ACTION, 1) * MIN_TIME);
+  pthread_mutex_unlock(&data->phi_st[data->id % NB_PHILO].chopstick);
+  pthread_mutex_unlock(&data->phi_st[(data->id + 1) % NB_PHILO].chopstick);
   rest(data);
 }
 
-void		think(t_data *data, int pos, int *count)
+static void	think(t_data *data, int pos)
 {
-  printf("[%d] think\n", data->id);
-  usleep(10000);
+  printf("\033[0;3%dm\t\t\t\t[%d] think\033[0m\n", data->id % 8 + 1, data->id);
+  usleep(MAX(TIME_ACTION, 1) * MIN_TIME);
   pthread_mutex_lock(&data->phi_st[pos].chopstick);
-  eat(data, count);
+  eat(data);
 }
 
-void		*start_diner(void *arg)
+static void	choose_action(t_data *data)
+{
+  if (!pthread_mutex_trylock(&data->phi_st[data->id % NB_PHILO].chopstick))
+    {
+      if (!pthread_mutex_trylock(&data->phi_st[(data->id + 1) % NB_PHILO].chopstick))
+	eat(data);
+      else
+	think(data, (data->id + 1) % NB_PHILO);
+    }
+  else if (!pthread_mutex_trylock(&data->phi_st[(data->id + 1) % NB_PHILO].chopstick))
+    {
+      if (!pthread_mutex_trylock(&data->phi_st[data->id % NB_PHILO].chopstick))
+	eat(data);
+      else
+	think(data, data->id % NB_PHILO);
+    }
+}
+
+static void	*start_diner(void *arg)
 {
   t_data	*data;
-  int		count;
 
-  count = 0;
   data = (t_data *)arg;
-  printf("Philosopher [%d] set around the table\n", data->id);
-  while (data->phi_st->food > 0)
-    {
-      if (!pthread_mutex_trylock(&data->phi_st[data->id % N_PHI].chopstick))
-	{
-	  if (!pthread_mutex_trylock(&data->phi_st[(data->id + 1) % N_PHI].chopstick))
-	    eat(data, &count);
-	  else
-	    think(data, (data->id + 1) % N_PHI, &count);
-	}
-      else if (!pthread_mutex_trylock(&data->phi_st[(data->id + 1) % N_PHI].chopstick))
-	{
-	  if (!pthread_mutex_trylock(&data->phi_st[data->id % N_PHI].chopstick))
-	    eat(data, &count);
-	  else
-	    think(data, data->id % N_PHI, &count);
-	}
-    }
-  printf("Philosopher [%d] has eaten %d time\n", data->id, count);
+  printf("\033[0;3%dmPhilosopher [%d] set around the table\033[0m\n", data->id % 8 + 1, data->id);
+  while (data->stat->food > 0)
+    choose_action(data);
   return (NULL);
 }
 
 int		main(void)
 {
-  t_data	data[N_PHI];
+  t_data	data[NB_PHILO];
+  t_stat	stat;
   int		i;
 
-  data->food = 100;
+  stat.food = NB_FOOD;
+  stat.total_eaten = 0;
+  if (pthread_mutex_init(&stat.food_lock, NULL))
+    return (EXIT_FAILURE);
   i = -1;
-  printf("%d\n", N_PHI);
-  while (++i != N_PHI)
+  printf("%d\n", NB_PHILO);
+  while (++i != NB_PHILO)
     {
       data[i].id = i;
       data[i].phi_st = data;
+      data[i].stat = &stat;
+      data[i].eaten_plates = 0;
       if (pthread_mutex_init(&data[i].chopstick, NULL))
 	return (EXIT_FAILURE);
     }
   i = -1;
-  while (++i != N_PHI)
+  while (++i != NB_PHILO)
     if (pthread_create(&data[i].thread, NULL, start_diner, &data[i]))
       return (EXIT_FAILURE);
   i = -1;
-  while (++i != N_PHI)
-    pthread_join(data[i].thread, NULL);
+  while (++i != NB_PHILO)
+    {
+      pthread_join(data[i].thread, NULL);
+      printf("\033[0;3%dmPhilosopher [%d] has eaten %d time\033[0m\n", i % 8 + 1, i, data[i].eaten_plates);
+    }
+  printf("Total eaten plates : %d\n", stat.total_eaten);
   return (EXIT_SUCCESS);
 }
